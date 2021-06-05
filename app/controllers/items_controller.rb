fb67de064be182b0d8aed61d83413ee15863f2f7
yu-dari <74702059+yu-dari@users.noreply.github.com>
2021-06-05 23:09:26 +0900

    商品出品機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -14,8 +14,7 @@ class ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_params)
-    if @item.valid?
-      @item.save
+    if @item.save
       redirect_to root_path
     else
       render :new
@@ -23,7 +22,6 @@ class ItemsController < ApplicationController
   end
 
   private
-  
 
   def item_params
     params.require(:item).permit(:name, :description, :category_id, :condition_id, :shipping_charge_id, :prefecture_id, :delivery_day_id, :price, :image
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -11,6 +11,7 @@ RSpec.describe Item, type: :model do
       it '全ての項目が入力されていれば出品ができる' do
         expect(@item).to be_valid
       end
+    end
 
     context '商品出品がうまくいかない時' do
       it 'imageが空では登録されない' do
@@ -29,22 +30,22 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Description can't be blank")
       end
       it 'category_idが空では登録されない' do
-        @item.category_id = nil
+        @item.category_id = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Category is not a number")
       end
       it 'condition_idが空では登録されない' do
-        @item.condition_id = nil
+        @item.condition_id = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Condition can't be blank", "Condition is not a number")
       end
       it 'prefecture_idが空では登録されない' do
-        @item.prefecture_id = nil
+        @item.prefecture_id = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Prefecture can't be blank", "Prefecture is not a number")
       end
       it 'delivery_day_idが空では登録されない' do
-        @item.delivery_day_id = nil
+        @item.delivery_day_id = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Delivery day can't be blank", "Delivery day is not a number")
       end
@@ -68,10 +69,21 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("Price must be less than 9999999")
       end
-      
-    end
-
+      it 'priceが半角英数混合では登録できない' do
+        @item.price = "100aaa"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
+      end
+      it 'priceが半角英語だけでは登録できない' do
+        @item.price = "aaaaaa"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
+      end
+      it 'ユーザーが紐付いていないと登録できない' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("User must exist") 
+      end
     end
   end
-
 end
