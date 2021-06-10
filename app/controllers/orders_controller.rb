f3f6b801ca25bee1f946444fa8ff94d537aeb628
yu-dari <74702059+yu-dari@users.noreply.github.com>
2021-06-10 13:23:25 +0900

    商品詳細表示機能

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,23 +2,23 @@ class OrdersController < ApplicationController
   before_action :authenticate_user!
   before_action :set_item, only: [:index, :create]
 
-  def index
-    if user_signed_in? && current_user.id != @item.user_id && @item.order == nil
-      @item_order = ItemOrder.new
-    else
-      redirect_to root_path
-    end
-  end
+  # def index
+  #   if user_signed_in? && current_user.id != @item.user_id && @item.order == nil
+  #     @item_order = ItemOrder.new
+  #   else
+  #     redirect_to root_path
+  #   end
+  # end
 
 
-  private
+  # private
 
-  def set_item
-    @item = Item.find(params[:item_id])
-  end
+  # def set_item
+  #   @item = Item.find(params[:item_id])
+  # end
 
-  def order_params
-    params.require(:item_order).permit(:postal_code, :prefecture_id, :city_name, :block_name, :building_name, :phone_number).merge(token: params[:token], item_id: params[:item_id], user_id: current_user.id )
-  end
+  # def order_params
+  #   params.require(:item_order).permit(:postal_code, :prefecture_id, :city_name, :block_name, :building_name, :phone_number).merge(token: params[:token], item_id: params[:item_id], user_id: current_user.id )
+  # end
 
 end
