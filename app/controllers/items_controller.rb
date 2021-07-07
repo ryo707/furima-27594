71c34216957ca31bb0314aea9e865115ca0a0bba
yu-dari <74702059+yu-dari@users.noreply.github.com>
2021-07-07 21:35:30 +0900

    訂正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -36,13 +36,13 @@ class ItemsController < ApplicationController
   end
 
 
-
   def destroy
     if @item.destroy
       redirect_to root_path
     end
   end
 
+  
   private
 
   def set_item
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,26 @@
 class Item < ApplicationRecord
+
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :description
+    with_options numericality: { other_than: 1, message: 'を選択してください' } do
+      validates :category_id
+      validates :condition_id
+      validates :shipping_charge_id
+      validates :prefecture_id
+      validates :delivery_day_id
+    end
+    validates :price, numericality: { only_integer: true }, inclusion: { in: 300..9_999_999, message: 'は300〜9,999,999の範囲内で入力してください' }
+  end
+
+  belongs_to :user
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_charge
+  belongs_to :prefecture
+  belongs_to :delivery_day
+  has_one_attached :image
+  has_one :order, dependent: :destroy
 end
