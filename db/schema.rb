e29582397fac9a37178ae15bc93b923acefed18c
yu-dari <74702059+yu-dari@users.noreply.github.com>
2021-07-12 02:29:43 +0900

    修正

--- db/schema.rb
+++ db/schema.rb
@@ -35,7 +35,7 @@ ActiveRecord::Schema.define(version: 2021_07_07_055012) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "postal_code", null: false
-    t.integer "prefectuire_id", null: false
+    t.integer "prefecture_id", null: false
     t.string "city", null: false
     t.string "street", null: false
     t.string "building"
